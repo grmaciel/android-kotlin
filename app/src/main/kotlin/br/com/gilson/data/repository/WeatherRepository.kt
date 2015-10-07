eac0667e11827bee3ab0e80f819b65cb859b9c09
Gilson Maciel <gmaciel@gmail.com>
2015-10-06 22:15:56 -0300

    Weather repository

--- app/src/main/kotlin/br/com/gilson/data/repository/WeatherRepository.kt
+++ app/src/main/kotlin/br/com/gilson/data/repository/WeatherRepository.kt
@@ -1,7 +1,18 @@
 package br.com.gilson.data.repository
 
+import br.com.gilson.data.ws.WeatherService
+import br.com.gilson.data.ws.dto.ForecastDto
+import rx.Observable
+import rx.schedulers.Schedulers
+
 /**
  * Created by Gilson Maciel on 06/10/2015.
  */
-class WeatherRepository {
+class WeatherRepository(val weatherService: WeatherService) {
+
+    public fun getForecast(dayCount: Int, city: String): Observable<ForecastDto> {
+        return weatherService.weatherForecast(dayCount, city)
+                .subscribeOn(Schedulers.newThread())
+                .flatMap { Observable.from(it) }
+    }
 }
